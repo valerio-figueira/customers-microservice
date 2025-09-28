fff0c6457d5237f62bf776c1697097d3bec332ca
José Valério <j.valerio.figueira@gmail.com>
2025-09-28 12:00:58 -0300

    fix eslint

--- src/core/domain/entities/policies/interfaces/document-policy.interface.ts
+++ src/core/domain/entities/policies/interfaces/document-policy.interface.ts
@@ -4,6 +4,6 @@ import { Document } from '../../document.entity';
 export interface DocumentPolicyInterface {
   ensureCustomerExists(customerId: string): Promise<void>;
   ensureDocumentIsUnique(document: DocumentInterface): Promise<void>;
-  ensureDocumentsAreUnique(documents: Document[]): Promise<void>
+  ensureDocumentsAreUnique(documents: Document[]): Promise<void>;
   ensureDocumentTypeIsUnique(document: DocumentInterface): Promise<void>;
 }
